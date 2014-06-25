7b5e146bfccebeca90e2474eb83ace6b590ee23a
Mincong Zhang <mincong.zhang@hotmail.com>
2014-06-25 19:31:11 +0100

    new

--- MATLAB_Verification/run_and_test.m
+++ MATLAB_Verification/run_and_test.m
@@ -1,4 +1,14 @@
 clc
-A = get_SH_descriptor('A.stl',0);
-B = get_SH_descriptor('A.stl',pi/6);
-whatever = get_SH_descriptor('femur.stl',0);
+
+Y_ml = spharm(1,1,pi/4,pi/4)
+
+P = sqrt(2)/2;
+N = sqrt(3/4/pi * 0.5);
+E = exp(4/pi * 1i);
+Y = P * N * E
+P*N
+%P*N*i^(m+abs(m)) matches SH = gsl_sf_legendre_sphPlm(1, 1, cos(pi/4));
+
+% A = get_SH_descriptor('A.stl',0);
+% B = get_SH_descriptor('A.stl',pi/6);
+% whatever = get_SH_descriptor('femur.stl',0);
