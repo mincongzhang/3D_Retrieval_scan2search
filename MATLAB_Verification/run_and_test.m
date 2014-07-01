02f370eb50c0fc90b919592fe1930daab7be044e
Mincong Zhang <mincong.zhang@hotmail.com>
2014-07-01 19:13:08 +0100

    Spherical harmonics finished in matlab

--- MATLAB_Verification/run_and_test.m
+++ MATLAB_Verification/run_and_test.m
@@ -16,18 +16,18 @@ clc
 %SH = gsl_sf_legendre_sphPlm(1, 1, cos(pi/4));
 
 %manual
-P = sqrt(2)/2;
-N = sqrt(3/4/pi * 0.5);
-m = 1;
-l = 1;
-SH_manual = P*N*i^(m+abs(m))
-
-%MATLAB
-SH_matlab = spharm(l,m,pi/4,pi/4)/exp(1i*m*pi/4)
+% P = sqrt(2)/2;
+% N = sqrt(3/4/pi * 0.5);
+% m = 0;
+% l = 0;
+% SH_manual = P*N*i^(m+abs(m))
+% 
+% %MATLAB
+% SH_matlab = spharm(l,m,pi/4,pi/4)/exp(1i*m*pi/4)
 
 
 
 %%
 % A = get_SH_descriptor('A.stl',0);
 % B = get_SH_descriptor('A.stl',pi/6);
-% whatever = get_SH_descriptor('femur.stl',0);
+B = get_SH_descriptor('femur.stl',pi/6);
