edaead17a1339449fe247091d03c83f8e160bf8b
Mincong Zhang <mincong.zhang@hotmail.com>
2014-07-14 02:08:24 +0100

    another symple but bad method

--- MATLAB_Verification/test_3D_rasteriztion.m
+++ MATLAB_Verification/test_3D_rasteriztion.m
@@ -5,53 +5,28 @@ function run()
     
     grid = zeros(10,10,10);
     p1 = [1,1,1];
-    p2 = [9,9,9];
-    p3 = [1,9,10];
-    max_x = max([p1(1),p2(1),p3(1)]);
-    max_y = max([p1(2),p2(2),p3(2)]);
-    max_z = max([p1(3),p2(3),p3(3)]);
-    min_x = min([p1(1),p2(1),p3(1)]);
-    min_y = min([p1(2),p2(2),p3(2)]);
-    min_z = min([p1(3),p2(3),p3(3)]);
-    x_len = max_x - min_x
-    y_len = max_y - min_y
-    z_len = max_z - min_z
-    
-    %返回最大len所在轴
-    max_on_axis = 1;
-    max_len = x_len;
-    if(y_len>max_len)
-        max_on_axis = 2;
-        max_len = y_len; 
+    p2 = [10,10,10];
+    p3 = [1,10,10];
+
+    [grid,coord12,len12] = fillGridLine(p1,p2,grid);
+    [grid,coord13,len23] = fillGridLine(p2,p3,grid);
+    [grid,coord23,len13] = fillGridLine(p1,p3,grid);
+
+    len12
+    len23
+    diff = len12-len23;
+    if(diff>=0)
+        for i = 1:len23
+            i
+            [grid,~,~] = fillGridLine(coord12(i,:),coord13(i,:),grid);
+        end
+        
+        for i = len23+1:len12
+            i
+            [grid,~,~] = fillGridLine(coord12(i,:),coord13(len23,:),grid);
+        end
+        
     end
-    if(z_len>max_len)
-        max_on_axis = 3;
-        max_len = z_len; 
-    end    
-    max_len
-    max_on_axis
-    
-    %排序，返回按其轴从大到小排列的3个点(C++里用stl的sort)
-    tmp = [p1(max_on_axis);p2(max_on_axis);p3(max_on_axis)];
-    pt_tmp = [p1;p2;p3]
-    pt = [];
-    for i = 1:3
-       idx = find(tmp == max(tmp))
-       tmp(idx)=[];
-       pt = [pt;pt_tmp(idx,:)]; 
-    end
-    pt
-    
-    
-    [coord12,grid] = fillGridLine(pt(1,:),pt(2,:),grid);
-    [coord13,grid] = fillGridLine(pt(2,:),pt(3,:),grid);
-    [coord23,grid] = fillGridLine(pt(1,:),pt(3,:),grid);
-    coord12
-    coord13
-    coord23
-    size(coord12)
-    size(coord13)
-    size(coord23)
     
     %get coordinates
     x_grid = [];
@@ -78,18 +53,21 @@ function run()
     
 end
 
-function [coord,grid] = fillGridLine(p1,p2,grid)
+function [grid,coord,len] = fillGridLine(p1,p2,grid)
 
     v12 = p2-p1;
     len_p12 = sqrt(v12(1)^2+v12(2)^2+v12(3)^2);
     v12_norm = v12./(len_p12);
     
     coord = [];
+    len = 0;
+    
     for i = 0:floor(len_p12)
         tmp_p = round(p1+v12_norm*i);
         if(grid(tmp_p(1),tmp_p(2),tmp_p(3))~=1)
             grid(tmp_p(1),tmp_p(2),tmp_p(3))=1;
             coord = [coord;tmp_p(1),tmp_p(2),tmp_p(3)];
+            len = len+1;
         end
     end
     grid(p2(1),p2(2),p2(3))=1;
