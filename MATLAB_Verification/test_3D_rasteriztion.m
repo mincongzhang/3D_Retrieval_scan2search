6591f1e446db033c8bfbc16a789bd2ba5d905417
Mincong Zhang <mincong.zhang@hotmail.com>
2014-07-26 16:51:49 +0100

    new

--- MATLAB_Verification/test_3D_rasteriztion.m
+++ MATLAB_Verification/test_3D_rasteriztion.m
@@ -8,22 +8,22 @@ function run()
     p2 = [10,10,10];
     p3 = [1,10,10];
 
-    [grid,coord12,len12] = fillGridLine(p1,p2,grid);
-    [grid,coord23,len23] = fillGridLine(p2,p3,grid);
-    [grid,coord13,len13] = fillGridLine(p1,p3,grid);
+    [grid,coord12,n_points12] = fillGridLine(p1,p2,grid);
+    [grid,coord23,n_points23] = fillGridLine(p2,p3,grid);
+    [grid,coord13,n_points13] = fillGridLine(p1,p3,grid);
 
-    len12
-    len13
-    diff = len12-len13;
+    n_points12
+    n_points13
+    diff = n_points12-n_points13;
     if(diff>=0)
-        for i = 1:len13
+        for i = 1:n_points13
             i
             [grid,~,~] = fillGridLine(coord12(i,:),coord13(i,:),grid);
         end
         
-        for i = len13+1:len12
+        for i = (n_points13+1):n_points12
             i
-            [grid,~,~] = fillGridLine(coord12(i,:),coord13(len13,:),grid);
+            [grid,~,~] = fillGridLine(coord12(i,:),coord13(n_points13,:),grid);
         end
         
     end
@@ -53,21 +53,21 @@ function run()
     
 end
 
-function [grid,coord,len] = fillGridLine(p1,p2,grid)
+function [grid,coord,n_points] = fillGridLine(p1,p2,grid)
 
     v12 = p2-p1;
     len_p12 = sqrt(v12(1)^2+v12(2)^2+v12(3)^2);
     v12_norm = v12./(len_p12);
     
     coord = [];
-    len = 0;
+    n_points = 0;
     
     for i = 0:floor(len_p12)
         tmp_p = round(p1+v12_norm*i);
         if(grid(tmp_p(1),tmp_p(2),tmp_p(3))~=1)
             grid(tmp_p(1),tmp_p(2),tmp_p(3))=1;
             coord = [coord;tmp_p(1),tmp_p(2),tmp_p(3)];
-            len = len+1;
+            n_points = n_points+1;
         end
     end
     grid(p2(1),p2(2),p2(3))=1;
